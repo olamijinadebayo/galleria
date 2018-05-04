94e195c30fb5e093c54d4f84cc2f045328c24308
olamijinadebayo <consultolamijin@gmail.com>
2018-05-04 17:22:03 +0300

    Project setup
    This will setup the models for the database,register the models in the admin.
    create a media root where the images will be stored.

--- galleria/photos/admin.py
+++ galleria/photos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Images,Category,Location
+
 
 # Register your models here.
+
+admin.site.register(Images)
+admin.site.register(Category)
+admin.site.register(Location)
--- galleria/photos/models.py
+++ galleria/photos/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category = models.CharField(max_length=20)
+
+class Location(models.Model):
+    country = models.CharField(max_length=20)
+
+class Images(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=20)
+    width = models.IntegerField(default=0)
+    height = models.IntegerField(default=0)
+    image = models.ImageField(upload_to ='images/', null=True,blank =False,width_field='width',height_field='height')
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+
+    def __str__(self):
+        return self.name
