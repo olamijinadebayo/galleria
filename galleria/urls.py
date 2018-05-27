4165897dc832318163354fa8ef05762653b2f1d0
olamijinadebayo <consultolamijin@gmail.com>
2018-05-27 23:27:50 +0300

    this will adjust the urls to ensure the page loads on the first page

--- galleria/urls.py
+++ galleria/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^photos/', include('photos.urls'))
+    url(r'', include('photos.urls'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
