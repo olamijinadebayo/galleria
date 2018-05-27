4bb0ab6efbe7fa5fa55245f6b4cd8561349faa2a
olamijinadebayo <consultolamijin@gmail.com>
2018-05-27 23:25:20 +0300

    this will edit the copy link and remove transform on images

--- galleria/urls.py
+++ galleria/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     url(r'^photos/', include('photos.urls'))
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- photos/views.py
+++ photos/views.py
@@ -1,31 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse,Http404
+from django.http import HttpResponse, Http404
 from .models import Images, Category, Location
 # Create your views here.
+
+
 def index(request):
     images = Images.all_images()
-    return render(request, 'main.html', {"images":images})
+    return render(request, 'main.html', {"images": images})
+
 
 def search(request):
-    print(request.GET)
-    print(request.GET)
     if 'category' in request.GET and request.GET["category"]:
         search_term = request.GET.get("category")
         searched_categories = Images.search_by_category(search_term)
         message = f"{search_term}"
 
-        return render(request, 'search.html',{"categories": searched_categories})
+        return render(request, 'search.html', {"categories": searched_categories})
 
     else:
         message = "You haven't searched for any term"
-    return render(request,'search.html')
+    return render(request, 'search.html')
+
 
 def kenya(request):
     locations = Images.kenyan_images()
-    print(locations)
-    return render(request, 'kenya.html',{"locations":locations})
+    return render(request, 'kenya.html', {"locations": locations})
+
 
 def nigeria(request):
     naija_pics = Images.nigerian_images()
     print(naija_pics)
-    return render(request, 'nigeria.html',{"naija_pics":naija_pics})
+    return render(request, 'nigeria.html', {"naija_pics": naija_pics})
